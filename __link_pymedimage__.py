783855568a10e77c67be43b20caeb35ff06dc526
Ryan Neph <neph320@gmail.com>
2018-07-06 10:28:30 -0700

    Delete __link_pymedimage__.py

--- __link_pymedimage__.py
+++
@@ -1,8 +0,0 @@
-import sys
-
-## Curently it is necessary to inform this project of the location where the separate PyMedImage library is installed
-## This can be done by setting the path to the git repository obtained by the command:
-##
-## git clone
-path = '../PyMedImage'
-sys.path.insert(0, path)
